57c79eeaaf117c7b4bb4e0d05c4e7edd369560b2
R13TechNewbie <ramadhanhidayat13@gmail.com>
2019-02-27 06:33:43 +0700

    Initial Commit

---
+++ exercise-2/javascript.js
@@ -0,0 +1,8 @@
+const kalkulator = document.querySelector('.kalkulator');
+const tombol = document.querySelector('.tombol-kalkulator');
+
+keys.addEventListener(‘click’, e => {
+    if (e.target.matches(‘button’)) {
+      // Do something
+    }
+   })
