a71e54ce346db4f2547c7b606de714a81b06164a
R13TechNewbie <ramadhanhidayat13@gmail.com>
2019-02-28 06:30:32 +0700

    tambahan javascript

--- exercise-2/javascript.js
+++ exercise-2/javascript.js
@@ -3,6 +3,34 @@ const tombol = document.querySelector('.tombol-kalkulator');
 
 keys.addEventListener(‘click’, e => {
     if (e.target.matches(‘button’)) {
-      // Do something
+      
+      const key = e.target
+      const action = key.dataset.action
+
+      if (!action) {
+        console.log('ini tombol angka')
+      }
+
+      if (
+        action === 'tambah' ||
+        action === 'kurang' ||
+        action === 'kali' ||
+        action === 'bagi' ||
+        action === 'modulo'
+      ) {
+        console.log('ini tombol operasi')
+      }
+
+      if (action === 'desimal') {
+        console.log('ini tombol desimal')
+      }
+
+      if (action === 'clear') {
+        console.log('ini tombol hapus')
+      }
+
+      if (action === 'hitung') {
+        console.log('tombol hitung')
+      }
     }
-   })
+})
