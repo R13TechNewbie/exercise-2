19f30833c99b2a3290698a7951844592208c35e9
R13TechNewbie <ramadhanhidayat13@gmail.com>
2019-02-28 18:57:25 +0700

    update dikit

--- exercise-2/javascript.js
+++ exercise-2/javascript.js
@@ -1,36 +1,38 @@
-const kalkulator = document.querySelector('.kalkulator');
-const tombol = document.querySelector('.tombol-kalkulator');
+const kalkulator = document.querySelector('.kalkulator')
+const tombol = kalkulator.querySelector('.tombol-kalkulator')
 
-keys.addEventListener(‘click’, e => {
-    if (e.target.matches(‘button’)) {
-      
-      const key = e.target
-      const action = key.dataset.action
+//if(tombol){
+  tombol.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        
+        const key = e.target
+        const action = key.dataset.action
 
-      if (!action) {
-        console.log('ini tombol angka')
+        if (!action) {
+          console.log('ini tombol angka')
+        }
+
+        if (
+          action === 'tambah' ||
+          action === 'kurang' ||
+          action === 'kali' ||
+          action === 'bagi' ||
+          action === 'modulo'
+        ) {
+          console.log(`ini tombol operasi`)
+        }
+
+        if (action === 'desimal') {
+          console.log('ini tombol desimal')
+        }
+
+        if (action === 'clear') {
+          console.log('ini tombol hapus')
+        }
+
+        if (action === 'hitung') {
+          console.log('tombol hitung')
+        }
       }
-
-      if (
-        action === 'tambah' ||
-        action === 'kurang' ||
-        action === 'kali' ||
-        action === 'bagi' ||
-        action === 'modulo'
-      ) {
-        console.log('ini tombol operasi')
-      }
-
-      if (action === 'desimal') {
-        console.log('ini tombol desimal')
-      }
-
-      if (action === 'clear') {
-        console.log('ini tombol hapus')
-      }
-
-      if (action === 'hitung') {
-        console.log('tombol hitung')
-      }
-    }
-})
+    });
+//}
